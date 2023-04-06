2d6d814a917165a2447b9a8bcf32482749bbf20b
Lukas Liesis <liesislukas@gmail.com>
2023-04-06 13:40:20 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,23 +1,20 @@
+"use strict";
 // Source: https://bost.ocks.org/mike/shuffle/
 // https://stackoverflow.com/a/2450976/1737158
-
-export default function arrayShuffle(array) {
-  if (array && Array.isArray(array) && array?.length > 0) {
-    let m = array.length,
-      t,
-      i;
-
-    // While there remain elements to shuffle…
-    while (m) {
-      // Pick a remaining element…
-      i = Math.floor(Math.random() * m--);
-
-      // And swap it with the current element.
-      t = array[m];
-      array[m] = array[i];
-      array[i] = t;
+Object.defineProperty(exports, "__esModule", { value: true });
+function arrayShuffle(array) {
+    if (array && Array.isArray(array) && (array === null || array === void 0 ? void 0 : array.length) > 0) {
+        var m = array.length, t = void 0, i = void 0;
+        // While there remain elements to shuffle…
+        while (m) {
+            // Pick a remaining element…
+            i = Math.floor(Math.random() * m--);
+            // And swap it with the current element.
+            t = array[m];
+            array[m] = array[i];
+            array[i] = t;
+        }
     }
-  }
-
-  return array;
+    return array;
 }
+exports.default = arrayShuffle;
